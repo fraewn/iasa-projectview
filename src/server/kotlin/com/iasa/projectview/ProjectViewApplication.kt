0745d0c42085fa72bd2bdc23ada6c073aa900090
Malte Esch <accounts@malteesch.de>
2020-04-10 12:51:37 +0100

    INITIAÃL COMMIT

---
+++ src/server/kotlin/com/iasa/projectview/ProjectViewApplication.kt
@@ -0,0 +1,11 @@
+package com.iasa.projectview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectViewApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjectViewApplication>(*args)
+}
---
+++ src/test/kotlin/com/iasa/projectview/ProjectViewApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.iasa.projectview
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectViewApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
