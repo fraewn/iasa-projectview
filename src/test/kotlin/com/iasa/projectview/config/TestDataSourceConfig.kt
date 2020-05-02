03d1e247ed0e17a796dc102f99cb540984f07da2
Malte Esch <accounts@malteesch.de>
2020-05-02 12:37:01 +0100

    Updated .gitlab-ci.yml and added TestDataSourceConfig

---
+++ src/test/kotlin/com/iasa/projectview/config/TestDataSourceConfig.kt
@@ -0,0 +1,28 @@
+package com.iasa.projectview.config
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.context.annotation.Profile
+import org.springframework.core.env.Environment
+import javax.sql.DataSource
+
+@Configuration
+@Profile("test")
+class TestDataSourceConfig(
+    @Autowired private val env: Environment
+) {
+
+    @Bean
+    @Primary
+    fun dataSource(): DataSource {
+        return DataSourceBuilder.create()
+            .driverClassName("org.postgresql.Driver")
+            .url("jdbc:postgresql://postgres:5432/${env.getRequiredProperty("POSTGRES_DB")}")
+            .username(env.getRequiredProperty("POSTGRES_USER"))
+            .password("")
+            .build()
+    }
+}
